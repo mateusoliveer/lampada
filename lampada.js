660dd9e8fb4f0163f85c8c6414d9fb571d156932
mateusoliveer <olivercorporativo90@gmail.com>
2023-10-16 22:46:42 -0300

    nova versao

--- lampada.js
+++ lampada.js
@@ -1,6 +1,7 @@
 const turnOn = document.getElementById ('turnOn');
 const turnOff = document.getElementById ('turnOff');
 const lamp = document.getElementById ('lamp');
+const consertar = document.getElementById('consertar');
 
 function isLampBroken () {
     return lamp.src.indexOf ( 'quebrada' ) > -1
@@ -28,8 +29,17 @@ function lampBroken () {
     lamp.src = './img/quebrada.png';
 }
 
+
+function consertarLampada () {
+    lamp.src = './img/desligada.png'
+ 
+}
+
+
+
 turnOn.addEventListener ( 'click', lampOn );
 turnOff.addEventListener ( 'click', lampOff );
 lamp.addEventListener ( 'mouseover', lampOn );
 lamp.addEventListener ('mouseleave', lampOff);
 lamp.addEventListener ('dblclick', lampBroken);
+consertar.addEventListener ( 'click', consertarLampada );
